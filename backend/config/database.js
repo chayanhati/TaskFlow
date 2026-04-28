f9dd649e1c7aa3215f898325bb35b447bbb7f52c
chayanhati <chayanhati2004@gmail.com>
2026-04-29 02:56:03 +0530

    fix database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,22 +1,16 @@
-const { Sequelize } = require('sequelize')
-require('dotenv').config()
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
+
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
   },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-})
+});
 
-module.exports = sequelize
+module.exports = sequelize;
